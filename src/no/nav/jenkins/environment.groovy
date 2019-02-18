ffa34f5db50ef0a0f29005a7928199a4b4d54814
Mayu-Thiyaga <39523522+Mayu-Thiyaga@users.noreply.github.com>
2019-02-18 13:52:38 +0100

    Create environment.groovy

---
+++ src/no/nav/jenkins/environment.groovy
@@ -0,0 +1,24 @@
+
+
+def setEnv() {
+    env.LANG = "nb_NO.UTF-8"
+    env.JAVA_HOME = "${tool 'jdk-1.8'}"
+    env.PATH = "${tool 'default-maven'}/bin:${env.PATH}"
+    env.ORACLE_HOME = "/u01/app/oracle/product/11.2.0/xe"
+    env.MAVEN_OPTS = "-Xms512m -Xmx1024m -XX:+TieredCompilation -XX:TieredStopAtLevel=1 "
+
+
+}
+
+def overrideJDK(String targetJDK) {
+    //Override default JDK
+    switch(targetJDK) {
+
+        case "11": env.JAVA_HOME = "${tool 'java-11'}"
+            break
+        case "10": env.JAVA_HOME = "${tool 'java-10'}"
+            break
+        case "1.8": env.JAVA_HOME = "${tool 'jdk-1.8'}"
+            break
+    }
+}
