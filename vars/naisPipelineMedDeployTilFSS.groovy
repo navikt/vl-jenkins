98e431358aebd6e13cb5033066ac6a400217d018
Martine Enger <marenger@student.matnat.uio.no>
2019-05-23 16:00:05 +0200

    kall på slack-script for å se om det hjelper

--- vars/naisPipelineMedDeployTilFSS.groovy
+++ vars/naisPipelineMedDeployTilFSS.groovy
@@ -3,6 +3,7 @@ import no.nav.jenkins.*
 def call() {
     def maven = new maven()
     def fpgithub = new fpgithub()
+    def slack = new slack()
     def version
     def githubRepoName
     def GIT_COMMIT_HASH_FULL
