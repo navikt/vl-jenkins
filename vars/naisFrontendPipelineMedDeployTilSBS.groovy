2d3fe215295ba4648c804852e83e0001a283d5a6
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-06-06 08:13:13 +0200

    Wrap input i timeout

--- vars/naisFrontendPipelineMedDeployTilSBS.groovy
+++ vars/naisFrontendPipelineMedDeployTilSBS.groovy
@@ -65,16 +65,15 @@ def call() {
                     branch 'master'
                 }
                 steps {
-                    input message: 'Vil du deploye master til prod?', ok: 'Ja, jeg vil deploye :)'
+                    timeout(time: 3, unit: 'DAYS') {
+                        input message: 'Vil du deploye master til prod?', ok: 'Ja, jeg vil deploye :)'
+                    }
                 }
             }
             stage('Deploy master til prod') {
                 agent { label 'master' }
                 when {
                     branch 'master'
-                }
-                options {
-                    timeout(time: 3, unit: 'DAYS')
                 }
                 steps {
                     script {
@@ -98,7 +97,9 @@ def call() {
                     }
                 }
                 steps {
-                    input message: 'Vil du deploye branch til preprod?', ok: 'Ja, jeg vil deploye :)'
+                    timeout(time: 30, unit: 'MINUTES') {
+                        input message: 'Vil du deploye branch til preprod?', ok: 'Ja, jeg vil deploye :)'
+                    }
                 }
             }
             stage('Deploy branch til preprod') {
@@ -107,9 +108,6 @@ def call() {
                     not {
                         branch 'master'
                     }
-                }
-                options {
-                    timeout(time: 30, unit: 'MINUTES')
                 }
                 steps {
                     script {
