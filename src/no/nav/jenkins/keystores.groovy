e2450b45e9afff6ff5e404496630fcdac5296fb3
Ola Loevholm <ola.lovholm@nav.no>
2019-06-21 14:45:29 +0200

    NOJIRA: Keystore-fiks flytte, rydde

--- src/no/nav/jenkins/keystores.groovy
+++ src/no/nav/jenkins/keystores.groovy
@@ -14,6 +14,8 @@ def generateKeystoreAndTruststore(){
 
     // local-host SSL
     sh(script: "rm -rf ${KEYSTORE_FOLDER}")
+    sh(script: "mkdir ${KEYSTORE_FOLDER}")
+
     sh(script: "openssl req -x509 -newkey rsa:2048 -keyout ${KEY_PEM} -out ${CERT_PEM} -days 365 -nodes -subj '/CN=localhost'", returnStdout: true)
     sh(script: "openssl pkcs12 -export -name localhost-ssl -in ${CERT_PEM} -inkey ${KEY_PEM} -out ${SERVERKEYSTORE} -password pass:${KEYSTORE_PASS}", returnStdout:true)
     sh(script: "keytool -importkeystore -destkeystore ${KEYSTORE_FILE} -srckeystore ${SERVERKEYSTORE} -srcstoretype pkcs12 -alias localhost-ssl -storepass ${KEYSTORE_PASS} -keypass ${KEYSTORE_PASS} -srcstorepass ${KEYSTORE_PASS}", returnStdout:true)
@@ -26,7 +28,10 @@ def generateKeystoreAndTruststore(){
 
     // truststore for SSL:
     sh(script: "keytool -import -trustcacerts -alias localhost-ssl -file ${CERT_PEM} -keystore ${TRUSTSTORE_FILE} -storepass ${TRUSTSTORE_PASS} -noprompt", returnStdout:true)
-    sh(script: "ls -la", returnStdout:true)
+
+    // Clean-up temporary files and move keystore and truststore
+    sh(script: "rm ${CERT_PEM} ${KEY_PEM} ${SERVERKEYSTORE}")
+    sh(script: "mv -t ${KEYSTORE_FOLDER} ${TRUSTSTORE_FILE} ${KEYSTORE_FILE}")
 
 
 }
