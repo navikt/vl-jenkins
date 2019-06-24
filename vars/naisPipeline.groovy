69afa87bd5416c4f0e99f7bce006c1deecdc79ed
Mayu-Thiyaga <39523522+Mayu-Thiyaga@users.noreply.github.com>
2019-06-24 16:15:56 +0200

    Deploymiljø er parametrisert.

--- vars/naisPipeline.groovy
+++ vars/naisPipeline.groovy
@@ -6,16 +6,26 @@ def call() {
     def version
     def githubRepoName
     def GIT_COMMIT_HASH_FULL
-    def artifactId
 
     pipeline {
         //agent any
-       agent { label 'MASTER' }
+        agent { label 'MASTER' }
+        parameters {
+          string(defaultValue: 't4', description: '', name: 'miljo')
+        }
+        options {
+          timestamps()
+        }
+        environment {
+            DOCKERREGISTRY = "repo.adeo.no:5443"
+            ARTIFACTID = readMavenPom().getArtifactId()
+        }
         stages {
 
             stage('Checkout scm') {
                 steps {
                     script {
+                        MILJO = params.miljo
                         Date date = new Date()
                         dockerRegistryIapp = "repo.adeo.no:5443"
 
@@ -26,7 +36,6 @@ def call() {
                         mRevision = maven.revision()
                         version = mRevision + changelist
                         githubRepoName = sh(script: "basename -s .git `git config --get remote.origin.url`", returnStdout: true).trim()
-                        artifactId = maven.artifactId()
                         currentBuild.displayName = version
 
                         echo "Building $version"
@@ -56,20 +65,20 @@ def call() {
 
                             envs = sh(returnStdout: true, script: 'env | sort -h').trim()
                             echo("envs: " + envs)
-                            echo("artifact: " +artifactId)
+                            echo("artifact: " + ARTIFACTID)
 
 
                             sh "mvn -B -Dfile.encoding=UTF-8 -DinstallAtEnd=true -DdeployAtEnd=true -Dsha1= -Dchangelist= -Drevision=$version clean install"
-                            if(artifactId.equalsIgnoreCase("fpmock2")){
+                            if(ARTIFACTID.equalsIgnoreCase("fpmock2")){
                                 echo("MVN deploy for fpmock2")
                                 sh "mvn -B -Dfile.encoding=UTF-8 -Dsha1= -Dchangelist= -Drevision=$version deploy"
                             }
-                            sh "docker build --pull -t $dockerRegistryIapp/$artifactId:$version ."
+                            sh "docker build --pull -t $dockerRegistryIapp/$ARTIFACTID:$version ."
                             withCredentials([[$class          : 'UsernamePasswordMultiBinding',
                                               credentialsId   : 'nexusUser',
                                               usernameVariable: 'NEXUS_USERNAME',
                                               passwordVariable: 'NEXUS_PASSWORD']]) {
-                                sh "docker login -u ${env.NEXUS_USERNAME} -p ${env.NEXUS_PASSWORD} ${dockerRegistryIapp} && docker push ${dockerRegistryIapp}/${artifactId}:${version}"
+                                sh "docker login -u ${env.NEXUS_USERNAME} -p ${env.NEXUS_PASSWORD} ${dockerRegistryIapp} && docker push ${dockerRegistryIapp}/${ARTIFACTID}:${version}"
                             }
                         }
                     }
@@ -103,9 +112,6 @@ def call() {
               }
               steps {
                   script {
-                
-                      MILJO = "t4"
-
                       dir ('k8s') {
                         def props = readProperties  interpolate: true, file: "application.${MILJO}.variabler.properties"
                         def value = "s/RELEASE_VERSION/${version}/g"
@@ -119,11 +125,11 @@ def call() {
                         } else {
                             naisNamespace = MILJO
                         }
-                        def exitCode=sh returnStatus: true, script: "k rollout status -n${naisNamespace} deployment/${artifactId}"
+                        def exitCode=sh returnStatus: true, script: "k rollout status -n${naisNamespace} deployment/${ARTIFACTID}"
                         echo "exit code is $exitCode"
 
                         if(exitCode == 0) {
-                            def veraPayload = "{\"environment\": \"${MILJO}\",\"application\": \"${artifactId}\",\"version\": \"${version}\",\"deployedBy\": \"Jenkins\"}"
+                            def veraPayload = "{\"environment\": \"${MILJO}\",\"application\": \"${ARTIFACTID}\",\"version\": \"${version}\",\"deployedBy\": \"Jenkins\"}"
                             def response = httpRequest([
                                     url                   : "https://vera.adeo.no/api/v1/deploylog",
                                     consoleLogResponseBody: true,
@@ -133,8 +139,7 @@ def call() {
                                     ignoreSslErrors       : true
                             ])
                         }
-                        addBadge icon: '', id: '', link: '', text: "${MILJO}-${version}"
-                      }
+                     }
                   }
               }
            }
