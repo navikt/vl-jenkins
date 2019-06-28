0e1dfe235e962bae3d901ce73ce5f66570c8452f
Ola Løvholm <ola.lovholm@nav.no>
2019-06-28 10:54:54 +0200

    NOJIRA: Groovy

--- src/no/nav/jenkins/keystores.groovy
+++ src/no/nav/jenkins/keystores.groovy
@@ -39,9 +39,9 @@ DNS.1 = fpmock2
     // local-host SSL
     sh(script: "rm -rf ${KEYSTORE_FOLDER}")
     sh(script: "mkdir ${KEYSTORE_FOLDER}")
-
     sh(script: "rm -f csr_config.txt", returnStdout: true)
-    sh(script: "echo ${CSR_DETAILS} >> csr_config.txt", returnStdout: true)
+    def file = new File("csr_config.txt")
+    file.write(CSR_DETAILS)
     sh(script: "openssl req -x509 -newkey rsa:2048 -keyout ${KEY_PEM} -out ${CERT_PEM} -days 365 -nodes -config csr_config.txt", returnStdout: true)
     sh(script: "openssl pkcs12 -export -name localhost-ssl -in ${CERT_PEM} -inkey ${KEY_PEM} -out ${SERVERKEYSTORE} -password pass:${KEYSTORE_PASS}", returnStdout:true)
     sh(script: "keytool -importkeystore -destkeystore ${KEYSTORE_FILE} -srckeystore ${SERVERKEYSTORE} -srcstoretype pkcs12 -alias localhost-ssl -storepass ${KEYSTORE_PASS} -keypass ${KEYSTORE_PASS} -srcstorepass ${KEYSTORE_PASS}", returnStdout:true)
