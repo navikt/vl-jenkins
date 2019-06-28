02b435e97f6bcfdb8044a468313aff6afcb839dc
Ola Løvholm <ola.lovholm@nav.no>
2019-06-28 10:33:00 +0200

    NOJIRA: Test av keystore CSR with alt-names

--- src/no/nav/jenkins/keystores.groovy
+++ src/no/nav/jenkins/keystores.groovy
@@ -12,11 +12,35 @@ def generateKeystoreAndTruststore(String cnName){
     def TRUSTSTORE_PASS = "changeit"
     def KEYSTORE_FOLDER = ".modig"
 
+    def CSR_DETAILS = """
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C=NO
+ST=Oslo
+L=Oslo
+O=Test TEst
+OU=Testing Domain
+emailAddress=test@vtp.com
+CN = ${cnName}
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = fpmock2
+"""
+
     // local-host SSL
     sh(script: "rm -rf ${KEYSTORE_FOLDER}")
     sh(script: "mkdir ${KEYSTORE_FOLDER}")
 
-    sh(script: "openssl req -x509 -newkey rsa:2048 -keyout ${KEY_PEM} -out ${CERT_PEM} -days 365 -nodes -subj '/CN=${cnName}'", returnStdout: true)
+    sh(script: "openssl req -x509 -newkey rsa:2048 -keyout ${KEY_PEM} -out ${CERT_PEM} -days 365 -nodes -config ${CSR_DETAILS}", returnStdout: true)
     sh(script: "openssl pkcs12 -export -name localhost-ssl -in ${CERT_PEM} -inkey ${KEY_PEM} -out ${SERVERKEYSTORE} -password pass:${KEYSTORE_PASS}", returnStdout:true)
     sh(script: "keytool -importkeystore -destkeystore ${KEYSTORE_FILE} -srckeystore ${SERVERKEYSTORE} -srcstoretype pkcs12 -alias localhost-ssl -storepass ${KEYSTORE_PASS} -keypass ${KEYSTORE_PASS} -srcstorepass ${KEYSTORE_PASS}", returnStdout:true)
     sh(script: "rm ${SERVERKEYSTORE}")
