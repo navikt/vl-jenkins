aeecc6e0d6326e1d23b43fbc43a7849d01b25430
Mayu-Thiyaga <39523522+Mayu-Thiyaga@users.noreply.github.com>
2019-09-03 14:56:39 +0200

    Skiller ut henting av versjon for en applikasjon

---
+++ src/no/nav/jenkins/nais.groovy
@@ -0,0 +1,31 @@
+package no.nav.jenkins
+
+def getAppVersion(context, ns, appl) {
+    def version
+    def dockerRegistryIapp = "repo.adeo.no:5443"
+    def dockerRegistryNav = "navikt"
+    deg msg
+    
+    sh "k config use-context $context"
+
+    def contImages = sh(
+       script: "k get pods -l app=${appl} -n${ns} -o jsonpath='{.items[*].spec.containers[*].image}'|tr -d '%'",
+       returnStdout: true
+    ).trim()
+    versions = contImages.replaceAll("$dockerRegistryIapp/$appl:", "").replaceAll("$dockerRegistryNav/$appl:", "").split()
+
+    if (versions) {
+      versions = versions.toUnique()
+      echo "versions: $ns $appl $versions"
+
+      if (versions.size() > 1) {
+        msg = "Endringer til P: $appl har feilende poder i $context $ns, sjekk!! "
+      } else if (versions.size() == 1) {
+        version = versions.first()
+      }
+    } else {
+      msg = "Endringer til P: $appl har ingen kjørende poder i $context $ns."
+    }
+
+    return [version, msg]
+}
