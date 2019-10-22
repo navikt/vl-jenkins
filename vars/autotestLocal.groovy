de21459ffd334c85cd7ea341a43f522ac56a13dc
olovholm <ola.lovholm@nav.no>
2019-10-22 10:56:27 +0200

    Legger til parameter for returnStdout.

--- vars/autotestLocal.groovy
+++ vars/autotestLocal.groovy
@@ -154,7 +154,7 @@ def call(body) {
                     sh(script: "echo EXTRA_CLASS_PATH=:vtp-lib/* >> sut.env")
 
                     def workspace = pwd()
-                    def host_ip = sh(script: "host a01apvl00312.adeo.no | sed 's/.*.\\s//'")
+                    def host_ip = sh(script: "host a01apvl00312.adeo.no | sed 's/.*.\\s//'", returnStdout: true)
                     println "Host :" + host_ip
 
                     sh "docker run -d --name $applikasjon --add-host=host.docker.internal:${host_ip} -v $workspace/.modig:/var/run/secrets/naisd.io/ --env-file sut.env  --env-file $workspace/resources/pipeline/autotest.list --env-file $workspace/resources/pipeline/" + params.applikasjon + "_datasource.list -p 8080:8080 -p 8000:8000 --link vtp:vtp " + dockerRegistry + "/$applikasjon:$sutToRun"
